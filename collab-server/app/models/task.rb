e21eb8cb3d82d734e50024ca2bcd3cc4d6cefab1
Kane Mathis <admin@kanemathis.com>
2022-12-02 10:43:23 -0500

    clean up code

--- collab-server/app/models/task.rb
+++ collab-server/app/models/task.rb
@@ -1,8 +1,10 @@
 class Task < ActiveRecord::Base
     belongs_to :project
-
+    # this is used in the GET request that populated the tables, it is in the "include: :user" that it is called
     def user
         id=self.user_id
         User.find(id)
     end
 end
+
+
